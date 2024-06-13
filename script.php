11144802962bb55fd59aa4927852e983d6591ee7
TheBeatboxer <efercitolove@gmail.com>
2024-06-13 00:19:34 -0500

    ga

--- script.php
+++ script.php
@@ -11,5 +11,5 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-
+echo "Conexión exitosa a la base de datos.";
 
